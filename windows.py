34631c72ec01ee18b12eb6714d9ffac3230adff3
jonathansayer <jon.sayer1@gmail.com>
2015-12-21 17:29:18 +0000

    create GUI

--- windows.py
+++ windows.py
@@ -1,2 +1,29 @@
-from Tkinter import Tk, Text
+from Tkinter import Tk, Text, BOTH
 from ttk import Frame, Button, Label, Style
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Windows")
+        self.pack(fill = BOTH, expand = True)
+
+        self.columnconfigure(1, weight = 1)
+        self.columnconfigure(3, pad = 7)
+        self.rowconfigure(3, weight = 1)
+        self.rowconfigure(5, pad = 5)
+
+        self.pack()
+
+def main():
+    root = Tk()
+    app = Example(root)
+    root.geometry("300x200+300+300")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
