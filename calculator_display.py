8271bb17e3e758cae2551a0d0ed7a1fb6579d2b4
jonathansayer <jon.sayer1@gmail.com>
2015-12-21 14:18:25 +0000

    created GUI window

--- calculator_display.py
+++ calculator_display.py
@@ -1,3 +1,26 @@
 from Tkinter import Tk, W, E
 from ttk import Frame, Button, Style
 from ttk import Entry
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self,parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Calculator")
+
+        Style().configure("TButton", padding = (0,5,0,5), font = 'serif 10')
+
+        self.pack()
+
+def main():
+    root = Tk()
+    root.geometry("300x200+300+300")
+    app = Example(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
