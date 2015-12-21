6ff8a3c85d4b5aca419bd48c25bbcefb5d94fa1e
jonathansayer <jon.sayer1@gmail.com>
2015-12-21 09:53:33 +0000

    added title label to frame1

--- review.py
+++ review.py
@@ -1,2 +1,30 @@
 from Tkinter import Tk, Text, TOP, BOTH, X, N, LEFT
 from ttk import Frame, Label, Entry
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Review")
+        self.pack(fill = BOTH, expand = True)
+
+        frame1 = Frame(self,)
+        frame1.pack(fill = X)
+
+        lbl1 = Label(frame1, text = "Title", width = 6)
+        lbl1.pack(side = LEFT, padx = 5, pady = 5)
+
+
+def main():
+    root = Tk()
+    root.geometry("300x300+300+300")
+    app = Example(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
