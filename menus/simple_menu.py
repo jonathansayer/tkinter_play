af4c2f35e7af27740a8b0b17f1dbd88f815c15fc
jonathansayer <jon.sayer1@gmail.com>
2015-12-23 13:29:37 +0000

    Changed dependencies to import all

--- menus/simple_menu.py
+++ menus/simple_menu.py
@@ -1,30 +1,37 @@
-from Tkinter import Tk, Frame, Menu
+from Tkinter import *
 
 class Example(Frame):
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
+
         self.parent = parent
         self.initUI()
 
+
     def initUI(self):
-        self.parent.title("Simple Menu")
+
+        self.parent.title("Simple menu")
 
         menubar = Menu(self.parent)
-        self.parent.config(menu = menubar)
+        self.parent.config(menu=menubar)
 
-        fileMenu = Menu(self.parent)
+        fileMenu = Menu(menubar)
         fileMenu.add_command(label = "Exit", command = self.onExit)
         menubar.add_cascade(label = "File", menu = fileMenu)
+
 
     def onExit(self):
         self.quit()
 
+
 def main():
+
     root = Tk()
-    app = Example(root)
     root.geometry("250x150+300+300")
+    app = Example(root)
     root.mainloop()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
