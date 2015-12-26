5bfc660e5a13d43bfe7c799ecc7898da61347592
jonathansayer <jon.sayer1@gmail.com>
2015-12-26 10:51:00 +0000

    added error button

--- dialogs/message_boxes.py
+++ dialogs/message_boxes.py
@@ -1,3 +1,29 @@
 from ttk import Frame, Button
 from Tkinter import Tk, BOTH
 import tkMessageBox as mbox
+
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+
+        self.parent.title("Message Box")
+        self.pack()
+
+        error = Button(self, text = "Error", command = self.onError)
+        error.grid(padx = 5, pady = 5)
+
+
+def main():
+    root = Tk()
+    app = Example()
+    root.geometry("300x150+300+300")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
