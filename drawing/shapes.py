8528f74d21fd13fc23b1ddd150a6cb99b456acda
jonathansayer <jon.sayer1@gmail.com>
2015-12-28 18:13:28 +0000

    added polygon

--- drawing/shapes.py
+++ drawing/shapes.py
@@ -17,6 +17,9 @@ class Example(Frame):
         canvas.create_rectangle(230, 10, 290, 60, outline = "gray", fill = "red", width = 5)
         canvas.create_arc(30, 200, 90, 100, start = 0, extent = 210, outline = "gray", fill = "gray", width = 2)
 
+        points = [150, 100, 200, 120, 240, 180, 210,200, 150, 150, 100, 200]
+        canvas.create_polygon(points, outline = "gray", fill = "gray", width = 2)
+
         canvas.pack(fill = BOTH, expand = 1)
 
 def main():
