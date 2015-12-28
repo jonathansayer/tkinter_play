086c782f60e942bb4857ad145f7f71114a0fa6df
jonathansayer <jon.sayer1@gmail.com>
2015-12-28 17:54:03 +0000

    create greyt oval

---
+++ drawing/shapes.py
@@ -0,0 +1,26 @@
+from Tkinter import Tk, Canvas, Frame, BOTH
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.parent.title("Shapes")
+        self.pack(fill = BOTH, expand = 1)
+
+        canvas = Canvas(self)
+        canvas.create_oval(10, 10, 80, 80, outline = "gray", fill = "gray", width = 2)
+
+        canvas.pack(fill = BOTH, expand = 1)
+
+def main():
+    root = Tk()
+    app = Example(root)
+    root.geometry("330x220+300+300")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
